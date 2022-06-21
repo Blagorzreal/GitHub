17b36375781e4e9845bb0397c211cdc58e30b444
blagorzreal <blagorzreal@gmail.com>
2022-06-21 16:40:01 +0300

    Added ResponseResult class.

---
+++ app/src/main/java/com/example/github/data/remote/ResponseResult.kt
@@ -0,0 +1,11 @@
+package com.example.github.data.remote
+
+sealed class ResponseResult<out T> {
+    abstract class ResponseError(var handled: Boolean = false): ResponseResult<Nothing>()
+
+    object EmptyResponseError: ResponseError()
+    object NullBodyResponseError: ResponseError()
+    data class ExceptionResponseError(val exception: Exception): ResponseError()
+    data class UnsuccessfulResponseError(val code: Int): ResponseError()
+    data class Success<out T>(val data: T): ResponseResult<T>()
+}
