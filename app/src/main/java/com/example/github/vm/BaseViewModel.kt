cafaad48d9da43a1bc9cb0690c208470668986e1
blagorzreal <blagorzreal@gmail.com>
2022-06-22 18:57:00 +0300

    Added BaseVM and moved isLoading, responseError and data fields. Added protected method to control the data fatching.

---
+++ app/src/main/java/com/example/github/vm/BaseViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.github.vm
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.github.data.remote.ResponseResult
+import com.example.github.util.log.AppLogger
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+abstract class BaseViewModel<D, M>(
+    private val mapper: (M) -> D,
+    private val tag: String,
+    protected val coroutineScope: CoroutineDispatcher = Dispatchers.IO): ViewModel() {
+
+    protected val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    protected val _responseError: MutableStateFlow<ResponseResult.ResponseError?> = MutableStateFlow(null)
+    val responseError: StateFlow<ResponseResult.ResponseError?> = _responseError
+
+    protected val _data: MutableStateFlow<D?> = MutableStateFlow(null)
+    val data: StateFlow<D?> = _data
+
+    init {
+        AppLogger.log(tag, "Initialized")
+    }
+
+    open fun onData(data: D) { }
+
+    open fun onError(error: ResponseResult.ResponseError) { }
+
+    protected fun fetchData(fetchAction: suspend () -> ResponseResult<M>) {
+        AppLogger.log(tag, "Fetching data")
+
+        _isLoading.value = true
+
+        viewModelScope.launch {
+            val result = withContext(coroutineScope) {
+                fetchAction()
+            }
+
+            _isLoading.value = false
+
+            when (result) {
+                is ResponseResult.Success -> {
+                    AppLogger.log(tag, "Fetched data successfully")
+
+                    val data = mapper(result.data)
+                    _data.value = data
+                    onData(data)
+                }
+                is ResponseResult.ResponseError -> {
+                    AppLogger.log(tag, "Unable to fetch the data: $result")
+
+                    _responseError.value = result
+                    onError(result)
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/github/vm/LoginViewModel.kt
+++ app/src/main/java/com/example/github/vm/LoginViewModel.kt
@@ -1,33 +1,23 @@
 package com.example.github.vm
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.github.data.data.UserData
 import com.example.github.data.remote.ApiProvider
-import com.example.github.data.remote.ResponseResult
 import com.example.github.data.remote.auth.IAuthApi
+import com.example.github.model.UserModel
 import com.example.github.ui.screen.UsernameValidationError
 import com.example.github.util.Constants.Companion.EMPTY_STRING
 import com.example.github.util.LoginHelper
 import com.example.github.util.log.AppLogger
 import com.example.github.util.log.LogType
-import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class LoginViewModel(
-    private val authApi: IAuthApi = ApiProvider.authApi,
-    private val coroutineScope: CoroutineDispatcher = Dispatchers.IO): ViewModel() {
+class LoginViewModel(private val authApi: IAuthApi = ApiProvider.authApi)
+    : BaseViewModel<UserData, UserModel>(LoginHelper::userModelToUserData, TAG) {
 
     companion object {
         private const val TAG = "LoginVM"
     }
-
-    private val _isLoading = MutableStateFlow(false)
-    val isLoading: StateFlow<Boolean> = _isLoading
-
-    private val _responseError: MutableStateFlow<ResponseResult.ResponseError?> = MutableStateFlow(null)
-    val responseError: StateFlow<ResponseResult.ResponseError?> = _responseError
 
     private val _validationError: MutableStateFlow<UsernameValidationError?> = MutableStateFlow(null)
     val validationError: StateFlow<UsernameValidationError?> = _validationError
@@ -35,16 +25,14 @@ class LoginViewModel(
     private val _username = MutableStateFlow(EMPTY_STRING)
     val username: StateFlow<String> = _username
 
-    private val _userData: MutableStateFlow<UserData?> = MutableStateFlow(null)
-    val userData: StateFlow<UserData?> = _userData
-
-    init {
-        AppLogger.log(TAG, "Initialized")
-    }
-
     fun onUsernameChanged(username: String) {
         AppLogger.log(TAG, "Username changed to $username")
         _username.value = username
+
+        if (validationError.value != null) {
+            AppLogger.log(TAG, "Reset username error")
+            _validationError.value = null
+        }
     }
 
     fun login() {
@@ -58,25 +46,6 @@ class LoginViewModel(
             return
         }
 
-        _isLoading.value = true
-
-        viewModelScope.launch {
-            val result = withContext(coroutineScope) {
-                authApi.login(usernameTrimmed)
-            }
-
-            _isLoading.value = false
-
-            when (result) {
-                is ResponseResult.Success -> {
-                    AppLogger.log(TAG, "Successfully logged in")
-                    _userData.value = LoginHelper.userModelToUserData(result.data)
-                }
-                is ResponseResult.ResponseError -> {
-                    AppLogger.log(TAG, "Unable to login: $result", logType = LogType.Error)
-                    _responseError.value = result
-                }
-            }
-        }
+        fetchData { authApi.login(usernameTrimmed) }
     }
 }
