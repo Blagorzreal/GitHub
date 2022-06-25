e6a4e0fc9a77a99463ad9f1c915437da58a796c5
blagorzreal <blagorzreal@gmail.com>
2022-06-25 15:36:26 +0300

    Impl. RepositoryScreen.

--- app/src/main/java/com/example/github/ui/screen/RepositoryScreen.kt
+++ app/src/main/java/com/example/github/ui/screen/RepositoryScreen.kt
@@ -1,9 +1,75 @@
 package com.example.github.ui.screen
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.example.github.R
 
 @Composable
-fun RepositoryScreen(owner: String, repo: String) {
-    Text("Hey, $owner of $repo")
+fun RepositoryScreen(navController: NavHostController, owner: String, repo: String) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = repo) },
+                navigationIcon = if (navController.previousBackStackEntry != null) {
+                    {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                } else null
+
+            )
+        },
+        content = {
+            Column(
+                Modifier
+                    .padding(it.calculateBottomPadding())
+                    .fillMaxWidth()) {
+                Box(
+                    modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = {  },
+                        shape = RoundedCornerShape(50.dp)
+                    ) {
+                        Text(text = owner)
+                    }
+                }
+
+                Box(
+                    modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+                ) {
+                    Button(
+                        onClick = {  },
+                        shape = RoundedCornerShape(50.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
+                    ) {
+                        Text(text = "Contributors")
+                    }
+                }
+
+                IconButton(onClick = { }) {
+                    Icon(
+                        modifier = Modifier.fillMaxSize().padding(50.dp),
+                        painter = painterResource(R.drawable.star),
+                        tint = Color.DarkGray,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
 }
