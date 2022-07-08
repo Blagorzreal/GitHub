963c751671d96204bb885af5a8414400b5b77bcc
blagorzreal <blagorzreal@gmail.com>
2022-07-08 19:16:23 +0300

    Clean up DaoProvider (should be deleted after the whole hilt/dagger migration).

--- app/src/main/java/com/example/github/data/local/DaoProvider.kt
+++ app/src/main/java/com/example/github/data/local/DaoProvider.kt
@@ -4,21 +4,12 @@ import android.content.Context
 import com.example.github.data.local.repos.IReposDao
 import com.example.github.data.local.repos.MockedReposDao
 import com.example.github.data.local.repos.ReposDBProvider
-import com.example.github.data.local.user.IUserDao
-import com.example.github.data.local.user.MockedUserDao
 import com.example.github.isMocked
 
 class DaoProvider private constructor() {
     companion object {
         fun init(context: Context) {
             ReposDBProvider.init(context)
-        }
-
-        val userDao: IUserDao by lazy {
-            if (isMocked)
-                MockedUserDao()
-            else
-                MockedUserDao()
         }
 
         val reposDao: IReposDao by lazy {
