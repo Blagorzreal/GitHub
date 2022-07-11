7b66cb7479e59ec3f051280e9a6aa414c12f7a0b
blagorzreal <blagorzreal@gmail.com>
2022-07-11 19:02:21 +0300

    Fixed unneeded creating of empty UserModel table in ReposDatabase.

--- app/src/main/java/com/example/github/data/local/repos/ReposDatabase.kt
+++ app/src/main/java/com/example/github/data/local/repos/ReposDatabase.kt
@@ -3,9 +3,8 @@ package com.example.github.data.local.repos
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.github.model.RepoModel
-import com.example.github.model.UserModel
 
-@Database(entities = [RepoModel::class, UserModel::class], version = 1)
+@Database(entities = [RepoModel::class], version = 1)
 abstract class ReposDatabase: RoomDatabase() {
     abstract fun reposDao(): ReposDao
 }
