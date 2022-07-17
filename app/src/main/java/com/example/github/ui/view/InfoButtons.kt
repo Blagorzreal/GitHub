2cbaa1d287f016d03e79b518dcdd5873a8cab8f8
blagorzreal <blagorzreal@gmail.com>
2022-07-17 17:03:49 +0300

    Fixed ugly background info buttons color for light theme.

--- app/src/main/java/com/example/github/ui/view/InfoButtons.kt
+++ app/src/main/java/com/example/github/ui/view/InfoButtons.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -44,8 +42,7 @@ private fun InfoButton(descriptionResource: Int, valueState: State<Long?>, onCli
     ) {
         Button(
             onClick = onClick,
-            shape = RoundedCornerShape(50.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = Color.DarkGray)
+            shape = RoundedCornerShape(50.dp)
         ) {
             Text(text = stringResource(descriptionResource, value))
         }
